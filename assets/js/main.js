bd14df4d41845bfa5cb836f686c8c3de1b8019e1
unknown <fabioflauzino@hotmail.com>
2022-07-24 23:31:55 -0300

    subindo javascript

---
+++ assets/js/main.js
@@ -0,0 +1,64 @@
+const pokemonName = document.querySelector('.pokemon__name');
+const pokemonNumber = document.querySelector('.pokemon__number');
+const pokemonImage = document.querySelector('.pokemon__image');
+const form = document.querySelector('.form');
+const input = document.querySelector('.input__search');
+const btnPrev = document.querySelector('.btn-prev');
+const btnNext = document.querySelector('.btn-next');
+
+let searchPokemon = 1;
+
+const fetchPokemon = async (pokemon) => {
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+    if(APIResponse.status === 200) {
+        const data = await APIResponse.json();
+        return data;
+    }
+}
+
+const renderPokemon = async (pokemon) => {
+
+    pokemonName.innerHTML = 'Carregando...';
+    pokemonNumber.innerHTML = '';
+
+    const data = await fetchPokemon(pokemon);
+
+    if(data){
+        pokemonName.innerHTML = data.name;
+        pokemonNumber.innerHTML = data.id;
+        if(pokemonImage.src == 'null'){
+            pokemonImage.src = './assets/img/favicon-16x16.png';
+        } else {
+            pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+        }
+                            
+        input.value = '';
+        searchPokemon = data.id;
+    } else {
+        pokemonName.innerHTML = 'Pokemon not found';
+        pokemonNumber.innerHTML = '';
+        pokemonImage.src = './assets/img/favicon-16x16.png';
+    }
+
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    renderPokemon(input.value.toLowerCase());
+})
+
+btnPrev.addEventListener('click', () => {
+    if(searchPokemon > 1){
+        searchPokemon -= 1;
+        renderPokemon(searchPokemon);
+    }
+})
+
+btnNext.addEventListener('click', () => {
+    searchPokemon += 1;
+    renderPokemon(searchPokemon);
+})
+
+renderPokemon(searchPokemon)
